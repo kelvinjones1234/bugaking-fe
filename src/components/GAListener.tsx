d623e5a2e3683212113bb58f8b16385ec0131c9b
kelvinjones1234 <praisejournal2@gmail.com>
2026-02-09 07:11:34 +0100

    added google analytics

--- src/components/GAListener.tsx
+++ src/components/GAListener.tsx
@@ -1,23 +1,23 @@
-// "use client";
+"use client";
 
-// import { usePathname, useSearchParams } from "next/navigation";
-// import { useEffect } from "react";
+import { usePathname, useSearchParams } from "next/navigation";
+import { useEffect } from "react";
 
-// const GA_ID = "G-GP814MESP6";
+const GA_ID = "G-GP814MESP6";
 
-// export default function GAListener() {
-//   const pathname = usePathname();
-//   const searchParams = useSearchParams();
+export default function GAListener() {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
 
-//   useEffect(() => {
-//     const gtag = (window as any).gtag;
+  useEffect(() => {
+    const gtag = (window as any).gtag;
 
-//     if (gtag) {
-//       gtag("config", GA_ID, {
-//         page_path: pathname + "?" + searchParams.toString(),
-//       });
-//     }
-//   }, [pathname, searchParams]);
+    if (gtag) {
+      gtag("config", GA_ID, {
+        page_path: pathname + "?" + searchParams.toString(),
+      });
+    }
+  }, [pathname, searchParams]);
 
-//   return null;
-// }
+  return null;
+}
